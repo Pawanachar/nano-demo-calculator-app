7c0a1c410284e3673546780a46627cad638f3e3f
Pawanachar <79529107+Pawanachar@users.noreply.github.com>
2023-08-22 12:35:47 +0530

    Update routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,45 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world"};
 }
+
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+
+    if (!input)
+    {
+        return crow::response(400, "Bad Request: Invalid JSON");
+    }
+
+    int a = input["a"].i();
+    int b = input["b"].i();
+    int result = a + b;
+
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+
+    return crow::response{responseJson};
 }
+
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+
+    if (!input)
+    {
+        return crow::response(400, "Bad Request: Invalid JSON");
+    }
+
+    int a = input["a"].i();
+    int b = input["b"].i();
+    int result = a - b;
+
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+
+    return crow::response{responseJson};
 }
+
+
